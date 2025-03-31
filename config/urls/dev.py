33fdd58e82b9fa9c48e85f36304d85f45bec2bb4
uuuj0821 <uuuj0821@naver.com>
2025-04-01 00:54:40 +0900

    run isort, black in local

--- config/urls/dev.py
+++ config/urls/dev.py
@@ -1,11 +1,17 @@
 # 개발 환경 설정
 from django.contrib import admin
-from django.urls import path, include
-from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+from django.urls import include, path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls), #관리자페이지
-    path('schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
-    path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    path("admin/", admin.site.urls),  # 관리자페이지
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"
+    ),
+    path("redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
--- config/urls/prod.py
+++ config/urls/prod.py
@@ -1,8 +1,8 @@
 # 배포 환경 설정
-from django.urls import path, include
 from django.contrib import admin
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),  # 관리 페이지
+    path("admin/", admin.site.urls),  # 관리 페이지
     # path('api/', include('yourapp.urls')),  # 앱의 URL들
 ]
