6844e589b91e97c1f3c85fbc3deaee90d013293d
uuuj0821 <uuuj0821@naver.com>
2025-04-01 01:56:44 +0900

    delete () in urls.py

--- config/urls/dev.py
+++ config/urls/dev.py
@@ -1,11 +1,7 @@
 # 개발 환경 설정
 from django.contrib import admin
 from django.urls import include, path
-from drf_spectacular.views import (
-    SpectacularAPIView,
-    SpectacularRedocView,
-    SpectacularSwaggerView,
-)
+from drf_spectacular.views import SpectacularAPIView,SpectacularRedocView,SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),  # 관리자페이지
